7566fb7e745962c475ec3a7a3e733b142b6e0919
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-04-20 19:01:01 +0200

    Update server.java

--- laboratorio_7/ejercicio_1/server.java
+++ laboratorio_7/ejercicio_1/server.java
@@ -1,50 +1,45 @@
-import java.io.*;
 import java.net.*;
-import java.util.Date;
+import java.io.*;
 
 public class Main
 {
-	public static void main(String args[])
-	{
+   public static void main(String[] args) throws IOException
+   {
+      int port = 9000; // Puerto del servidor en escucha
+      ServerSocket serverSocket = new ServerSocket(port); // Creacion del server socket
+      while(true)
+      {
+         Socket clientSocket = serverSocket.accept(); // Espera a que un cliente se conecte (Socket de Bienvenida)
 
-		ServerSocket servidor = null;
-		Socket sc = null;
+         // Gracias a la clase PrintWriter podemos escribir datos formateados en el flujo de salida del socket de cliente
+         // Mediante esta clase podemos enviar datos a travez de este flujo de salida el cual sera la salida
+         // correspondiente a este ejercicio que es en si enviar informacion del socket cliente y el mensaje que el
+         // cliente nos envie
 
-		DataInputStream in;
-		DataOutputStream out;
+         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-		final int port = 9876;
+         // BufferedReader nos ayudara a leer los datos de un flujo en este caso del flujo del socket cliente
+         // Mediante la variable in obtendremos todo el flujo de bits que nos envie el cliente
 
-		try
-		{
-			servidor = new ServerSocket(port);
+         BufferedReader in = new BufferedReader(
+             new InputStreamReader(clientSocket.getInputStream()));
+         try
+         {
+            String mensaje_a_cliente;
 
-			System.out.println("Servidor iniciado");
+            mensaje_a_cliente = "IP Cliente: "+ clientSocket.getInetAddress().getHostAddress() + "\nPuerto: " + Integer.toString(clientSocket.getPort());
 
-			while(true)
-			{
-				sc = servidor.accept();
-
-				System.out.println("Cliente conectado");
-
-				in = new DataInputStream(sc.getInputStream());
-				out = new DataOutputStream(sc.getOutputStream());
-
-				String mensaje = in.readUTF();
-
-				System.out.println(mensaje);
-
-				out.writeUTF("Hola como estas xD?");
-
-				sc.close();
-
-			}
-
-		}
-		catch(IOException ex)
-		{
-
-		}
-		
-	}
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) // Esperando a que el cliente nos envie datos mediante el flujo entrante
+            { 
+               mensaje_a_cliente += "\nMensaje del cliente: " + inputLine;
+               out.println(mensaje_a_cliente); // Mediante el flujo de salida enviamos el mensaje al cliente
+            }
+         }
+         catch (IOException e)
+         {
+            System.out.println(e.getMessage());
+         }
+      }
+   }
 }
